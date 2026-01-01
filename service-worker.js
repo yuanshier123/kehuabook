19dd2b6119331d021b0d017a2bcc0f5aa45c060a
yuanshier123 <1992664435@qq.com>
2026-01-01 14:00:57 +0800

    fix(pages): make service worker paths relative for GitHub Pages and register SW

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,34 @@
-// Service Worker for offline support
 const CACHE_NAME = 'yike-photobook-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/assets/',
-  // Add other static resources as needed
+  './',
+  './index.html',
+  './assets/',
+  './service-worker.js'
 ];
 
-// Install event - cache static resources
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Activate event - clean up old caches
 self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    caches.keys().then((cacheNames) => Promise.all(
+      cacheNames.map((cacheName) => {
+        if (cacheWhitelist.indexOf(cacheName) === -1) {
+          return caches.delete(cacheName);
+        }
+      })
+    ))
   );
 });
 
-// Fetch event - serve from cache if available
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        // Return cached response if found
-        if (response) {
-          return response;
-        }
-        // Otherwise fetch from network
-        return fetch(event.request);
-      })
+      .then((response) => response || fetch(event.request))
   );
 });
